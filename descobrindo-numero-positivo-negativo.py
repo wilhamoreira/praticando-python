fe51776cb483764e871fade152fdd13b4076689b
Wilha Moreira <wilhadev@gmail.com>
2022-12-05 20:41:35 -0300

    primeiro envio

---
+++ descobrindo-numero-positivo-negativo.py
@@ -0,0 +1,16 @@
+'''
+Receba um número e verifique se ele é positivo ou negatio
+
+'''
+#limpando o terminal
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+#recebendo um numero inteiro
+numero = int(input('Informe um numero: '))
+
+#fazendo as validações do numero para saber se é positivo ou negativo
+if numero >= 0 :
+    print(f'O numero {numero} é um numero POSITIVO')
+else:
+    print(f'O numero {numero} é um numero NEGATIVO')
